ac6eec78ff85f0e3f27b093a4563b3cbba4eb636
NessMC <contact@nessmc.fr>
2020-06-06 11:00:31 +0200

    ✨ Substraction function added.

--- math/src/main.rs
+++ math/src/main.rs
@@ -2,6 +2,11 @@ fn add (number1: i32, number2: i32) -> i32 {
     return number1 + number2;
 }
 
+fn sub (number1: i32, number2: i32) -> i32 {
+    return number1 - number2;
+}
+
 fn main() {
     println!("3 + 5 = {}", add(3, 5));
+    println!("6 - 2 = {}", sub(6, 2));
 }
