45c81e3e1f314fffad42f3bbdd2d7e67f2fba19d
NessMC <contact@nessmc.fr>
2020-06-06 11:01:29 +0200

    ✨ Division function added.

--- math/src/main.rs
+++ math/src/main.rs
@@ -6,7 +6,13 @@ fn sub (number1: i32, number2: i32) -> i32 {
     return number1 - number2;
 }
 
+fn div (number1: i32, number2: i32) -> i32 {
+    return number1 / number2;
+}
+
 fn main() {
     println!("3 + 5 = {}", add(3, 5));
     println!("6 - 2 = {}", sub(6, 2));
+    println!("12 * 15 = {}", sub(6, 2));
+    println!("24 / 8 = {}", div(24, 8));
 }
