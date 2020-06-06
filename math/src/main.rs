6cbf50bf5ad27dda759746994207b3281354ba36
NessMC <contact@nessmc.fr>
2020-06-06 11:01:56 +0200

    ✨ Multiplication function added.

--- math/src/main.rs
+++ math/src/main.rs
@@ -10,9 +10,13 @@ fn div (number1: i32, number2: i32) -> i32 {
     return number1 / number2;
 }
 
+fn mul (number1: i32, number2: i32) -> i32 {
+    return number1 * number2;
+}
+
 fn main() {
     println!("3 + 5 = {}", add(3, 5));
     println!("6 - 2 = {}", sub(6, 2));
-    println!("12 * 15 = {}", sub(6, 2));
+    println!("12 * 15 = {}", mul(12, 15));
     println!("24 / 8 = {}", div(24, 8));
 }
