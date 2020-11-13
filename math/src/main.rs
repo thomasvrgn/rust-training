1e5e7a9928bf13581e41568b1c1b5714a84bbee3
thomasvergne <thomas@quark-lang.dev>
2020-11-13 14:00:00 +0100

    🧼 Code cleaned up.

--- math/src/main.rs
+++ math/src/main.rs
@@ -1,22 +1,22 @@
-fn add (number1: i32, number2: i32) -> i32 {
-    return number1 + number2;
+fn add(number1: i32, number2: i32) -> i32 {
+  return number1 + number2;
 }
 
-fn sub (number1: i32, number2: i32) -> i32 {
-    return number1 - number2;
+fn sub(number1: i32, number2: i32) -> i32 {
+  return number1 - number2;
 }
 
-fn div (number1: i32, number2: i32) -> i32 {
-    return number1 / number2;
+fn div(number1: i32, number2: i32) -> i32 {
+  return number1 / number2;
 }
 
-fn mul (number1: i32, number2: i32) -> i32 {
-    return number1 * number2;
+fn mul(number1: i32, number2: i32) -> i32 {
+  return number1 * number2;
 }
 
 fn main() {
-    println!("3 + 5 = {}", add(3, 5));
-    println!("6 - 2 = {}", sub(6, 2));
-    println!("12 * 15 = {}", mul(12, 15));
-    println!("24 / 8 = {}", div(24, 8));
+  println!("3 + 5 = {}", add(3, 5));
+  println!("6 - 2 = {}", sub(6, 2));
+  println!("12 * 15 = {}", mul(12, 15));
+  println!("24 / 8 = {}", div(24, 8));
 }
